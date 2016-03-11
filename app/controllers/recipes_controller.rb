3909a75557096aef34ea2b47f36511f3bd2abdc7
gpdno@mac.com <gpdno@mac.com>
2016-03-11 04:03:40 +0000

    :Added recipes page and new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,30 @@ class RecipesController <ApplicationController
     @recipes = Recipe.all
   end
   
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(3)
+    
+    if @recipe.save
+      flash[:success] = "The recipe was created"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
 end
