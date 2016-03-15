fbc34cee5c54b2c8ebbb47aa488c7c86a926888a
gpdno@mac.com <gpdno@mac.com>
2016-03-15 00:26:23 +0000

    added likes and dislikes to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController <ApplicationController
   
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
   end
   
   def show
@@ -36,6 +36,18 @@ class RecipesController <ApplicationController
       redirect_to recipe_path(@recipe)
     else
       render :edit
+    end
+  end
+  
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your vote was recorded"
+      redirect_to :back
+    else
+      flash[:warning] = "Sorry, you only have one vote per item"
+      redirect_to :back
     end
   end
   
